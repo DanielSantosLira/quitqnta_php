ba76d7b29be146a4e4314c8d2d6fa12848c7b93c
Daniel Santos Pereira Lira <danielsplira@gmail.com>
2023-05-19 11:37:16 -0300

    novo

---
+++ quemsomos.php
@@ -0,0 +1,123 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/node_modules/bootstrap/dist/css/bootstrap.css">
+    <link rel="shortcut icon" href="img/banana-favicon.png" type="image/x-icon">
+    <title>Quitanda Online :: Quem Somos</title>
+
+    
+
+</head>
+
+<body style="min-width:372px;">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-danger border-bottom shadow-sm mb-3">
+        <div class="container">
+            <a class="navbar-brand" href="">
+                <strong>Quitanda Online</strong>
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="navbar-collapse collapse">
+                <ul class="navbar-nav flex-grow-1">
+                    <li class="nav-item">
+                        <a href="/index.html" class="nav-link text-white">Principal</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/contato.html" class="nav-link text-white">Contato</a>
+                    </li>
+                </ul>
+                <div class="align-self-end">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a href="/cadastro.html" class="nav-link text-white">Quero Me Cadastrar</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="/login.html" class="nav-link text-white">Entrar</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="/carrinho.html" class="nav-link text-white">
+                                <svg class="bi" width="24" height="24" fill="currentColor">
+                                    <use xlink:href="/bi.svg#cart3" />
+                                </svg>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    <main>
+        <div class="container">
+            <h1>Quem Somos</h1>
+            <hr>
+            <p>
+                Quitanda Online é uma loja virtual (e-commerce) fictícia criada como estudo de caso para o curso "Desenvolvimento de uma Loja Virtual com Bootstrap 5 e ASP.NET 5" disponibilizado gratuitamente pelo Prof. Ricardo Maroquio em seu canal no YouTube.
+            </p>
+            <p>
+                O código-fonte pode ser obtido via GitHub, conforme orientações apresentadas ao longo dos vídeos do curso no YouTube. Este estudo de caso pode ser utilizado livremente como base para outras lojas virtuais, sem ônus para o usuário.
+            </p>
+            <p>
+                Bons estudos e boa programação!
+            </p>
+        </div>
+    </main>
+
+    <div style="height: 273px;" class="d-block d-md-none"></div>
+    <div style="height: 153px;" class="d-none d-md-block d-lg-none"></div>
+    <div style="height: 129px;" class="d-none d-lg-block"></div>
+
+    <footer class="border-top fixed-bottom text-muted bg-light">
+
+        <div class="container">
+
+             <div class="row py-3">
+
+                  <div class="col-12 col-md-4 text-center text-md-left text-secondary">
+                       &copy;2023 - Quintanda Online Ltda Me<br>
+                       Rua Virtual Inexistente, 171, Compulãndia/PC<br>
+                       CNPJ 99.999.999/0001-99
+                  </div>
+
+                  <div class="col-12 col-md text-center">
+
+                       <a href="privacidade.html" class="text-decoration-none text-secondary">Política de
+                            Privacidade</a><br>
+                       <a href="termos.html" class="text-decoration-none text-secondary">Termos de Usu</a><br>
+                       <a href="quemsomos.html" class="text-decoration-none text-secondary">Quem Somos</a><br>
+                       <a href="troca.html" class="text-decoration-none text-secondary">Troca e Devoluçôes</a>
+                  </div>
+
+                  <div class="col-12 col-md-4 text-center text-md-right">
+                       <a href="contato" class="text-decoration-none text-secondary ">Contato pelo site</a><br>
+                       <svg width="15" height="15" fill="currentColor" class="bi">
+                            <use xlink:href="/bi.svg#envelope" class="text-primary">
+                       </svg>
+                       E-mail:<a href="danielsplira@gmail.com"
+                            class="text-decoration-none text-secondary">danielsplira@gmail.com</a><br>
+                       <svg width="15" height="15" fill="currentColor" class="bi text-danger">
+                            <use xlink:href="/bi.svg#telephone">
+                       </svg>
+                       Telefone:<a href="contato" class="text-decoration-none text-secondary">(19) 9 94078406</a><br>
+                       <svg width="15" height="15" fill="currentColor" class="bi text-success">
+                            <use xlink:href="/bi.svg#whatsapp">
+                       </svg>
+                       Whatsapp:<a href="contato" class="text-decoration-none text-secondary">(19) 9 94078406</a><br>
+
+
+                  </div>
+
+             </div>
+
+        </div>
+
+   </footer>
+
+    <script src="/node_modules/bootstrap/dist/js/bootstrap.bundle.js"></script>
+</body>
+
+</html>
---
+++ troca.php
@@ -0,0 +1,194 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+
+     <meta charset="utf-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+
+     <link rel="stylesheet" href="/node_modules/bootstrap/dist/css/bootstrap.css">
+     <title>Quitanda Online :: Trocas e Devoluçôes</title>
+     <link rel="shortcut icon" href="img/banana-favicon.png" type="image/x-icon">
+     <link rel="stylesheet" href="/node_modules/bootstrap/dist/css/bootstrap.css">
+     <title>Quitanda Online :: Principal</title>
+     <link rel="shortcut icon" href="img/banana-favicon.png" type="image/x-icon">
+     <link rel="shortcut icon" href="img/banana-favicon.png" type="image/x-icon">
+     <link rel="aplle-touch-icon" sizes="180x180" href="/img/favicon/aplle-touch-icon.png">
+     <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon/favicon-32x32.png">
+     <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon/favicon-16x16.png">
+     <link rel="manifest"  href="/img/favicon/site.webmanifest">
+     <link rel="mask-icon"  href="/img/favicon/safari-pinned-tab.sgv" color="#5bbad5">
+     <link rel="shortcut icon"  href="/img/favicon/favicon-icon">
+     <meta name="msapplication-TileColor" content="#da532c">
+     <meta name="msapplication-config" content="/img/favicon/browserconfig.xml">
+     <meta name="thame-color" content="#ffffff">
+
+
+
+</head>
+
+<body style="min-width:372px;">
+     <nav class="navbar navbar-expand-lg navbar-dark bg-danger border-bottom shadow-sm mb-3">
+
+          <div class="container">
+
+               <a href="#" class="navbar-brand"><strong>Quintanda Online</strong></a>
+
+               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target=".navbar-collapse">
+
+                    <span class="navbar-toggler-icon"></span>
+
+               </button>
+
+               <div class="navbar-collapse collapse">
+
+                    <ul class="navbar-nav flex-grow-1">
+
+                         <li class="nav-item">
+                              <a href="#" class="nav-link text-white">Principal</a>
+                         </li>
+
+                         <li class="nav-item">
+                              <a href="#" class="nav-link text-white">Contatos</a>
+                         </li>
+                    </ul>
+
+
+                    <div class="align-self-end">
+
+                         <ul class="navbar-nav">
+
+                              <li class="nav-item">
+                                   <a href="#" class="nav-link text-white">Quero Me Cadastrar</a>
+                              </li>
+
+                              <li class="nav-item">
+                                   <a href="#" class="nav-link text-white">Entrar</a>
+                              </li>
+
+                              <li class="nav-item">
+                                   <a href="#" class="nav-link text-white">
+                                        <svg class="bi" width="24" height="24" fill="currentColor">
+                                             <use xlink:href="/bi.svg#cart3" />
+
+                                        </svg>
+                                   </a>
+                              </li>
+                         </ul>
+                    </div>
+               </div>
+          </div>
+     </nav>
+
+
+
+     <main>
+          <div class="container">
+               <h1> POLÍTICA DE TROCA, DEVOLUÇÃO E ARREPENDIMENTO</h1>
+               <hr>
+
+               <p>
+                   A Quitanda online utiliza tecnologia de ponta para a fabricação de seus produtos, primando pela
+                    qualidade
+                    e satisfação de seus clientes. Pelo respeito e para que seja mantida a credibilidade conquistada
+                    junto
+                    aos seus consumidores, a empresa criou uma política de troca e devolução de acordo com o Código de
+                    Defesa
+                    do Consumidor, e preocupada para que você (cliente) obtenha uma negociação eficaz, ágil e
+                    principalmente
+                    satisfatória.
+               </p>
+               <p>
+                    Caso opte pelo contato via correio eletrônico ou telefônico, será encaminhado a você o formulário
+                    para
+                    preenchimento e envio junto à(s) peça(s). O produto devolvido sem esse formulário e/ou sem a
+                    comunicação
+                    ao SAC será reenviado sem consulta prévia.
+               </p>
+               <p>
+                    Ao efetuar o processo de devolução/troca o cliente deverá no verso da nota fiscal a ser
+                    devolvida/trocada, informar o motivo da devolução/troca, o nome de quem está devolvendo, CPF e a
+                    data da
+                    devolução.
+               </p>
+               <p>
+                <strong>*ATENÇÃO:</strong> Para efetuar o processo de troca é necessário estar logado. Devolução por
+                    Arrependimento/Desistência Se ao receber o produto, você resolver devolvê-lo por arrependimento,
+                    deverá
+                    fazê-lo em até sete dias corridos, a contar da data de recebimento.
+               </p>
+               <p>
+                    Observando as seguintes condições: O produto não poderá ter indícios de uso.
+               </p>
+
+               <p>
+                    O produto deverá ser encaminhado preferencialmente na embalagem original, acompanhado de nota
+                    fiscal,
+                    etiquetas, tags (etiqueta com código de referência do produto) devidamente fixada no produto e todos
+                    os
+                    seus acessórios.
+               </p>
+               <p>
+                    Ao efetuar o processo de devolução o cliente deverá no verso da nota fiscal a ser devolvida,
+                    informar o
+                    motivo da recusa/devolução, o nome de quem está devolvendo, CPF e a data da devolução.
+               </p>
+          </div>
+
+     </main>
+
+     <div style="height: 273px;" class="d-block d-md-none"></div>
+     <div style="height: 153px;" class="d-none d-md-block d-lg-none"></div>
+     <div style="height: 129px;" class="d-none d-lg-block"></div>
+
+
+     <footer class="border-top fixed-bottom text-muted bg-light">
+
+          <div class="container">
+
+               <div class="row py-3">
+
+                    <div class="col-12 col-md-4 text-center text-md-left text-secondary">
+                         &copy;2023 - Quintanda Online Ltda Me<br>
+                         Rua Virtual Inexistente, 171, Compulãndia/PC<br>
+                         CNPJ 99.999.999/0001-99
+                    </div>
+
+                    <div class="col-12 col-md text-center">
+
+                         <a href="privacidade.html" class="text-decoration-none text-secondary">Política de
+                              Privacidade</a><br>
+                         <a href="termos.html" class="text-decoration-none text-secondary">Termos de Usu</a><br>
+                         <a href="quensomos.html" class="text-decoration-none text-secondary">Quem Somos</a><br>
+                         <a href="troca.html" class="text-decoration-none text-secondary">Troca e Devoluçôes</a>
+                    </div>
+
+                    <div class="col-12 col-md-4 text-center text-md-right">
+                         <a href="contato" class="text-decoration-none text-secondary ">Contato pelo site</a><br>
+                         <svg width="15" height="15" fill="currentColor" class="bi">
+                              <use xlink:href="/bi.svg#envelope" class="text-primary">
+                         </svg>
+                         E-mail:<a href="danielsplira@gmail.com"
+                              class="text-decoration-none text-secondary">danielsplira@gmail.com</a><br>
+                         <svg width="15" height="15" fill="currentColor" class="bi text-danger">
+                              <use xlink:href="/bi.svg#telephone">
+                         </svg>
+                         Telefone:<a href="contato" class="text-decoration-none text-secondary">(19) 9 94078406</a><br>
+                         <svg width="15" height="15" fill="currentColor" class="bi text-success">
+                              <use xlink:href="/bi.svg#whatsapp">
+                         </svg>
+                         Whatsapp:<a href="contato" class="text-decoration-none text-secondary">(19) 9 94078406</a><br>
+
+
+                    </div>
+
+               </div>
+
+          </div>
+
+     </footer>
+
+     <script src="\node_modules\bootstrap\dist\js\bootstrap.bundle.js"></script>
+</body>
+
+</html>
